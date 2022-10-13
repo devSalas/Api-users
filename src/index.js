94db67ad48ddf93cbe365d1213acba678afd68b5
esteban salas <esalassulca@gmail.com>
2022-10-13 18:23:44 -0500

    agregando dependencia cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express= require('express')
 const v1router= require('./v1/routes/userRoutes')
-
+const cors=require("cors")
 const app= express()
 const PORT= process.env.PORT || 3000
-
+app.use(cors)
 app.use("/api/v1/users",v1router)
 
 
