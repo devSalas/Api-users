59b3a1bb4195902f64fe1fa26dce4463f1b0cde4
esteban salas <esalassulca@gmail.com>
2022-10-13 20:53:14 -0500

    problemas  con el puerto, no reconoce el puerto railway

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const v1router= require('./v1/routes/userRoutes')
 const cors=require("cors")
 const app= express()
 
-const PORT=process.env.PORT ||  3000 
+const PORT= process.env.PORT || 3000 
 
 app.use(cors())
 app.use(express.json())
