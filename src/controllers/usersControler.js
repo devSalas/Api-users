98f4c8297f182f09d6616e2b38a5f0fe1f1d00a8
esteban salas <esalassulca@gmail.com>
2022-10-15 21:04:17 -0500

    solucionado error en el controlador con la funcion createNewUser, faltaba convertir a json la respuesta

--- src/controllers/usersControler.js
+++ src/controllers/usersControler.js
@@ -13,7 +13,7 @@ const getOneUser =async (req,res)=>{
 }
 const createNewUser =async (req,res)=>{
   const {body}= req;
-  if(!body.name ) return res.send("ha fallado, tal vez porque no ingresaste datos, como el nombre");
+  if(!body.name ) return res.send({error:"ha fallado, tal vez porque no ingresaste datos, como el nombre"});
   const newUser={
     name:body.name,
   }
